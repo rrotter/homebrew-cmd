8c30d9abc0658ebaad3baf0d3864af738129d323
Ryan Rotter <rrotter@users.noreply.github.com>
2024-07-04 21:38:11 -0400

    taps command: add help text, improve formatting
    - use "-" in place of "0" for readability
    - bold headers and symbols
    - tap names are hyperlinks
      - requires iTerm or other terminal that supports OSC 8 Anchors

--- cmd/taps.rb
+++ cmd/taps.rb
@@ -7,28 +7,64 @@ module Homebrew
   module Cmd
     class TapsCmd < AbstractCommand
       cmd_args do
+        usage_banner "`taps` [--verbose]"
+        description <<~EOS
+          Print table of installed taps and tap details.
+
+          Tap details:
+            Tap: cannonical name of tap
+            Size: output of `du -sh` for tap path
+            🍺: Number of formulae available from tap
+            🍷: Number of casks available from tap
+            Cmd: Number of commands available from tap
+
+          Extra data: symbols after final column indicate...
+            `*` - tap has uncommitted changes
+            `!` - tap is not a git repo, or has no `remote`/`origin`
+            (foo) - tap is currently on branch "foo"
+        EOS
+      end
+
+      def a(text, href, pad_to = 10)
+        pad = [pad_to - text.size, 0].max
+        "\033]8;;#{href}\007#{text}\033]8;;\007" + Array.new(pad, " ").join
+      end
+
+      def fmt_i(i)
+        i.zero? ? "-" : i.to_s
       end
 
       def run
         taps = Tap.all
-        name_col = taps.map{|x| x.name.size}.max
-        fmt = "%-#{name_col}s %4s %4s %4s %3s %s"
-        puts( sprintf("%-#{name_col}s %4s ", "Tap", "Size") + "  🍺   🍷 Cmd")
+        name_col = taps.map(&:name).max_by(&:size).size
+        fmt = "%s %4s %4s %4s %3s %s"
+        puts "\033[1mTap\033[0m#{Array.new(name_col-3, " ").join} \033[1mSize\033[0m   🍺   🍷 \033[1mCmd\033[0m"
 
         taps.sort_by(&:name).each do |tap|
           extra = []
 
           if tap.installed?
-            extra << tap.path if args.verbose?
-            extra << "(#{tap.git_repository.branch_name})" unless tap.git_repository.default_origin_branch?
-            dirty = %x[cd #{tap.path}; git status --porcelain].strip
-            extra << "*" unless dirty.strip.empty?
             size = `du -sh #{tap.path}`.split.first
+            extra << tap.path if args.verbose?
+
+            if tap.remote
+              dirty = `cd #{tap.path}; git status --porcelain`.strip
+              extra << "\033[1m*\033[0m" unless dirty.strip.empty?
+              extra << "(#{tap.git_repository.branch_name})" unless tap.git_repository.default_origin_branch?
+            else
+              extra << "\033[1m!\033[0m"
+            end
           else
             size = "-"
           end
 
-          puts sprintf fmt, tap.name, size, tap.formula_files.size, tap.cask_files.size, tap.command_files.size, extra.join(" ")
+          puts sprintf fmt,
+                       a(tap.name, tap.remote || "file:///#{tap.path}", name_col),
+                       size,
+                       fmt_i(tap.formula_files.size),
+                       fmt_i(tap.cask_files.size),
+                       fmt_i(tap.command_files.size),
+                       extra.join(" ")
         end
       end
     end
